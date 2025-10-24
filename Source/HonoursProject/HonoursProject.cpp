08f33e0c5d871850c7c47acb16d5ba371a289567
PaRK66M <2200637@uad.ac.uk>
2025-10-24 14:10:38 +0100

    Initial Project Commit
    Default project creation with IDE plugin and platformer template project

---
+++ Source/HonoursProject/HonoursProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "HonoursProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HonoursProject, "HonoursProject" );
